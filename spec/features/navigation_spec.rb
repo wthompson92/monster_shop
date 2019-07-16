e14f02d46d00baf6251c975a2889dcb128b4357c
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-07-15 21:01:33 -0600

    Add to navigation feature spec for nav bar navigation user story 2

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -16,6 +16,29 @@ RSpec.describe 'Site Navigation' do
       end
 
       expect(current_path).to eq('/merchants')
+
+      within 'nav' do
+        click_link "Cart:"
+      end
+
+      expect(current_path).to eq('/cart')
+
+      within 'nav' do
+        click_link 'Log In'
+      end
+
+      expect(current_path).to eq(login_path)
+
+      within 'nav' do
+        click_link 'User Regestration'
+      end
+        expect(current_path).to eq(new_user_path)
+
+      within 'nav' do
+        click_link 'Home'
+      end
+
+      expect(current_path).to eq('/merchants')
     end
 
     it 'I see a cart indicator in my nav bar' do
@@ -25,5 +48,33 @@ RSpec.describe 'Site Navigation' do
         expect(page).to have_content("Cart: 0")
       end
     end
+
+    # it 'I see a link to my cart in my nav bar' do
+    #   within 'nav' do
+    #     click_link "Cart: #{@cart.count}"
+    #   end
+    #   expect(current_path).to eq('/cart')
+    # end
+
+    # it 'I see a link to log in' do
+    #   within 'nav' do
+    #     click_link 'Log In'
+    #   end
+    #   expect(current_path).to eq('/login')
+    # end
+
+    # it 'I see a link to the user registration page' do
+    #   within 'nav' do
+    #     click_link 'User Regestration'
+    #   end
+    #   expect(current_path).to eq('/register')
+    # end
+
+    # it 'I see a link to return to the welcome / home page' do
+    #   within 'nav' do
+    #     click_link 'Home'
+    #   end
+    #   expect(current_path).to eq('/merchants')
+    #end
   end
 end
