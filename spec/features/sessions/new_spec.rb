0e1d34072ed81cf7259054bc2415c10d3b493b87
username <wnt925@gmail.com>
2019-07-16 20:56:07 -0600

    Adding happy path Login test

--- spec/features/sessions/new_spec.rb
+++ spec/features/sessions/new_spec.rb
@@ -1,21 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "User Login" do
-    before :each do
-    visit new_user_path
-    fill_in "User name", with: "jsmith123"
-    fill_in "Password", with: "password123"
-    fill_in "address", with: "123 Main St"
-    click_button "Create Profile"
+  describe "As a Visitor" do
+  before :each do
+  visit new_user_path
+  fill_in "Name", with: "John Smith"
+  fill_in "User name", with: "jsmith123"
+  fill_in "Password", with: "password123"
+  fill_in "Password confirmation", with: "password123"
+  fill_in "Address", with: "123 Main St"
+  fill_in "City", with: "Denver"
+  fill_in "State", with: "Colorado"
+  fill_in "Zip", with: 80501
+  click_button "Create Profile"
+  @new_user = User.last
   end
 
-  it "logs an existing user in and redirects them to the user show page." do
+  it "When I click the link to login I am taken to a login form" do
     visit root_path
   click_link "Login"
+  expect(current_path).to eq(login_path)
   fill_in "User name", with: "jsmith123"
   fill_in "Password", with: "password123"
   click_button "Log In"
-  expect(current_path).to eq(user_path(current_user))
-  # expect(page).to have_content("user_name: [\"can't be blank\"]")
+  expect(current_path).to eq(user_path(@new_user))
   end
+end
 end
