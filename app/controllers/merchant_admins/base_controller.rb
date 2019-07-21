8c5e7c295533b014cbb7969d2b50e3cc243b2ed9
Jori Peterson <joripeterson@gmail.com>
2019-07-21 14:04:09 -0600

    Refactor for proper functionality

--- app/controllers/merchant_admins/base_controller.rb
+++ app/controllers/merchant_admins/base_controller.rb
@@ -1,5 +1,5 @@
 class MerchantAdmins::BaseController < ApplicationController
-  before_action :require_merchant_admin || :require_merchant_employee
+  before_action :require_merchant_access
 
   private
     def require_merchant_admin
@@ -10,6 +10,12 @@ class MerchantAdmins::BaseController < ApplicationController
 
 		def require_merchant_employee
 			unless current_merchant_employee?
+				render file: "/public/404", status: 404
+			end
+		end
+
+		def require_merchant_access
+			unless current_merchant_employee? || current_merchant_admin?
 				render file: "/public/404", status: 404
 			end
 		end
