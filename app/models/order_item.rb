1241cd5ef3106b1fb3353875c4d79e0927b4c6c2
username <wnt925@gmail.com>
2019-07-23 16:50:10 -0600

    Add all

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -6,4 +6,11 @@ class OrderItem < ApplicationRecord
     quantity * price
   end
 
+   def subtract_inventory
+     inventory - quantity
+   end
+
+   def fulfill_order
+       self.update_attributes(fulfilled: true)
+   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 User.destroy_all
 Merchant.destroy_all
 Item.destroy_all
 Review.destroy_all
 Order.destroy_all
 OrderItem.destroy_all
-
 @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
 @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
 @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
@@ -26,21 +17,12 @@ OrderItem.destroy_all
 @troll = @brian.items.create!(name: 'Troll', description: "I'm a Troll", price: 55, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 20 )
 @ghoul = @brian.items.create!(name: 'Ghoul', description: "I'm a Ghoul", price: 99, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
 @unicorn = @brian.items.create!(name: 'Unicorn', description: "I'm a Unicorn!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: false, inventory: 8 )
-@megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
-@brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
-@ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20.25, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
-@giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
-@hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
-
-@order_1 = @user_1.orders.create!
-@order_2 = @user_1.orders.create!
-@order_1.order_items.create!(item: @ogre, price: @ogre.price, quantity: 2)
-@order_1.order_items.create!(item: @hippo, price: @hippo.price, quantity: 3)
-@order_2.order_items.create!(item: @hippo, price: @hippo.price, quantity: 2)
 
 
-@jori = User.create!(user_name: "jpeterson", password: "123",  password_confirmation: "123", role: 0, name: "Jori Peterson", address: "123 Main St", city: "Westminster", state: "Colorado", zip: 80791)
-@nathan = User.create!(user_name: "nthomas", password: "123",  password_confirmation: "123", role: 0, name: "Nathan Thomas", address: "123 Main St", city: "Gunbarrel", state: "Colorado", zip: 80301, merchant_id: @megan.id)
-@andrew = User.create!(user_name: "ajohnson", password: "123",  password_confirmation: "123", role: 1, name: "Andrew Johnson", address: "123 Main St", city: "Lakewood", state: "Colorado", zip: 80401, merchant_id: @megan.id)
-@will = User.create!(user_name: "wthomson", password: "123",  password_confirmation: "123", name: "Will Thompson", address: "123 Main St", city: "Longmont", state: "Colorado", zip: 80501)
+
+
+@jori = User.create!(user_name: "jpeterson", password: "123", password_confirmation: "123", role: 0, name: "Jori Peterson", address: "123 Main St", city: "Westminster", state: "Colorado", zip: 80791)
+@nathan = User.create!(user_name: "nthomas", password: "123", password_confirmation: "123", role: 0, name: "Nathan Thomas", address: "123 Main St", city: "Gunbarrel", state: "Colorado", zip: 80301, merchant_id: @megan.id)
+@andrew = User.create!(user_name: "ajohnson", password: "123", password_confirmation: "123", role: 1, name: "Andrew Johnson", address: "123 Main St", city: "Lakewood", state: "Colorado", zip: 80401, merchant_id: @megan.id)
+@will = User.create!(user_name: "wthomson", password: "123", password_confirmation: "123", name: "Will Thompson", address: "123 Main St", city: "Longmont", state: "Colorado", zip: 80501)
 @admin123 = User.create!(user_name: "andrew@gmail.com", password: "thinking123", role: 2, name: "Andrew", address: "333 Market St", city: "Denver", state: "CO", zip: 80012 )
