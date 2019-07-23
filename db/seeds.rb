db4549753dbf532ffb21ece57dca56ca68fa6f73
username <wnt925@gmail.com>
2019-07-23 16:46:57 -0600

    Add

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,16 +32,15 @@ OrderItem.destroy_all
 @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
 @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
 
+@order_1 = @user_1.orders.create!
+@order_2 = @user_1.orders.create!
+@order_1.order_items.create!(item: @ogre, price: @ogre.price, quantity: 2)
+@order_1.order_items.create!(item: @hippo, price: @hippo.price, quantity: 3)
+@order_2.order_items.create!(item: @hippo, price: @hippo.price, quantity: 2)
+
 
 @jori = User.create!(user_name: "jpeterson", password: "123",  password_confirmation: "123", role: 0, name: "Jori Peterson", address: "123 Main St", city: "Westminster", state: "Colorado", zip: 80791)
 @nathan = User.create!(user_name: "nthomas", password: "123",  password_confirmation: "123", role: 0, name: "Nathan Thomas", address: "123 Main St", city: "Gunbarrel", state: "Colorado", zip: 80301, merchant_id: @megan.id)
 @andrew = User.create!(user_name: "ajohnson", password: "123",  password_confirmation: "123", role: 1, name: "Andrew Johnson", address: "123 Main St", city: "Lakewood", state: "Colorado", zip: 80401, merchant_id: @megan.id)
 @will = User.create!(user_name: "wthomson", password: "123",  password_confirmation: "123", name: "Will Thompson", address: "123 Main St", city: "Longmont", state: "Colorado", zip: 80501)
 @admin123 = User.create!(user_name: "andrew@gmail.com", password: "thinking123", role: 2, name: "Andrew", address: "333 Market St", city: "Denver", state: "CO", zip: 80012 )
-
-
-# @order_1 = @user_1.orders.create!
-# @order_2 = @user_1.orders.create!
-# @order_1.order_items.create!(item: @ogre, price: @ogre.price, quantity: 2)
-# @order_1.order_items.create!(item: @hippo, price: @hippo.price, quantity: 3)
-# @order_2.order_items.create!(item: @hippo, price: @hippo.price, quantity: 2)
