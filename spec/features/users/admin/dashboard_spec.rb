790e9b32a1e1e0f1de254cdf29cf56b83c7b671e
username <wnt925@gmail.com>
2019-07-23 23:33:21 -0600

    Add passing tests

--- spec/features/users/admin/dashboard_spec.rb
+++ spec/features/users/admin/dashboard_spec.rb
@@ -1,28 +1,66 @@
-
 require 'rails_helper'
 
 RSpec.describe "Admin Index" do
-  describe "As an admin" do
     before :each do
+      @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+
+      @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+
+      @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
+
+      @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+
+      @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 75, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+
       @admin = User.create!(user_name: "andrew@gmail.com", password: "thinking123", role: 2, name: "Andrew", address: "333 Market St", city: "Denver", state: "CO", zip: 80012 )
       @nathan = User.create!(user_name: "nthomas", password: "123",  password_confirmation: "123", name: "Nathan Thomas", address: "123 Main St", city: "Gunbarrel", state: "Colorado", zip: 80301)
       @andrew = User.create!(user_name: "ajohnson", password: "123",  password_confirmation: "123", name: "Andrew Johnson", address: "123 Main St", city: "Lakewood", state: "Colorado", zip: 80401)
       @jori = User.create!(user_name: "jpeterson", password: "123",  password_confirmation: "123", name: "Jori Peterson", address: "123 Main St", city: "Westminster", state: "Colorado", zip: 80791)
       @will = User.create!(user_name: "wthomson", password: "123",  password_confirmation: "123", name: "Will Thompson", address: "123 Main St", city: "Longmont", state: "Colorado", zip: 80501)
+      @merchant_admin = User.create!(user_name: "nathan@gmail.com", password: "password123", role: 1, name: "Nathan", address: "123 Market St", city: "Denver", state: "CO", zip: 80012, merchant_id: @megan.id )
+
+      @order_1 = @nathan.orders.create!
+
+      @order_item_1 = @order_1.order_items.create!(price: @ogre.price, quantity: 3, item: @ogre)
+      @order_item_2 = @order_1.order_items.create!(price: @giant.price, quantity: 1000, item: @giant)
+      @order_item_3 = @order_1.order_items.create!(price: @hippo.price, quantity: 2, item: @hippo)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
       visit admin_dashboard_path
     end
 
-    describe "When I login to my account and click the link Admin Dashboard"do
+    describe "When I login to my account and click the link Admin Dashboard" do
       it  "I am taken a dashboard page with all active orders" do
       visit root_path
       click_link "Admin Dashboard"
       expect(current_path).to eq(admin_dashboard_path)
       end
 
-      it  "I see a ship button next to all packeged orders. When I press the ship button, the status is changed to shipped and the user cannont cancel the order." do
+      it "For each order I see the following information: the user who placed the order, which links to admin view of user profile, order id, date the order was created" do
+
+        expect(page).to have_link(@nathan.user_name)
+        expect(page).to have_content(@order_1.id)
+        expect(page).to have_content(@order_1.created_at.to_formatted_s(:short))
+        
       end
+      ## it  "I see a ship button next to all packeged orders. When I press the ship button, the status is changed to shipped and the user cannont cancel the order." do
+      # end
+
+#       As an admin user
+# When I log into my dashboard, "/admin/dashboard"
+# Then I see all orders in the system.
+# For each order I see the following information:
+#
+# - user who placed the order, which links to admin view of user profile
+# - order id
+# - date the order was created
+#
+# Orders are sorted by "status" in this order:
+#
+# - packaged
+# - pending
+# - shipped
+# - cancelled
+
     end
   end
-end
---
+++ spec/features/users/merchant_admin/orders/show_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+RSpec.describe "Merchant admin order sho" do
+  describe "As a merchant admin" do
+    before :each do
+
+      @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+
+      @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+
+      @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
+
+      @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+
+      @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 75, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+
+      @nathan = User.create!(user_name: "nthomas", password: "123",  password_confirmation: "123", name: "Nathan Thomas", address: "123 Main St", city: "Gunbarrel", state: "Colorado", zip: 80301)
+
+
+      @merchant_admin = User.create!(user_name: "nathan@gmail.com", password: "password123", role: 1, name: "Nathan", address: "123 Market St", city: "Denver", state: "CO", zip: 80012, merchant_id: @megan.id )
+
+      @order_1 = @nathan.orders.create!
+
+      @order_item_1 = @order_1.order_items.create!(price: @ogre.price, quantity: 3, item: @ogre)
+      @order_item_2 = @order_1.order_items.create!(price: @giant.price, quantity: 1000, item: @giant)
+      @order_item_3 = @order_1.order_items.create!(price: @hippo.price, quantity: 2, item: @hippo)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_admin)
+      visit merchant_admins_order_path(@order_1)
+
+    end
+
+    describe "When I visit an order show page from my dashboard" do
+
+      it "I see the customer's name and address" do
+        expect(page).to have_content(@nathan.name)
+        expect(page).to have_content(@nathan.address)
+        # save_and_open_page
+      end
+
+      it "I only see the items in the order that are being purchased from my merchant and do not see items in the order from other merchants." do
+        expect(page).to have_content(@ogre.name)
+        expect(page).to have_content(@giant.name)
+        expect(page).to_not have_content(@hippo.name)
+
+      end
+
+      it "For each item I see the name of the Item, whis is a link to the item show page and  the price" do
+        expect(page).to have_link(@ogre.name)
+        expect(page).to have_link(@giant.name)
+        expect(page).to_not have_link(@hippo.name)
+        expect(page).to have_content(@ogre.price)
+        expect(page).to have_content(@giant.price)
+        expect(page).to_not have_content(@hippo.price)
+
+      end
+
+
+      describe "If the user's desired quantity is greater than my current inventory quantity for that item" do
+        it "I do not see a fulfill button or link. Instead I see a notice next to the item indicating I cannot fulfill this item"  do
+
+          within "#item-#{@ogre.id}" do
+          expect(page).to have_button("Fulfill Order")
+        end
+
+          within "#item-#{@giant.id}" do
+          expect(page).to_not have_button("Fulfill Order")
+          expect(page).to have_content("You do not have a high enough inventory to fulfill the order")
+        end
+        end
+      end
+    end
+  end
+end
