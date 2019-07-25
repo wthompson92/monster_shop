3feeb23ede2dd009112af7fe4144ef1865d84d12
Jori Peterson <joripeterson@gmail.com>
2019-07-24 18:14:26 -0600

    Erase order_items controller

--- app/controllers/merchant_admins/order_items_contoller.rb
+++
@@ -1,9 +0,0 @@
-class MerchantAdmins::OrderItemsController < MerchantAdmins::BaseController
-
-    def update
-      @merchant = Merchant.find(current_user.merchant_id)
-      @order_items  = @merchant.order_items
-      @fulfill = @order_items.fulfill_order
-      @sub = @order_items.subtract_inventory
-      redirect_to merchant_admins_order_path(@order_items.order_id)
-  	end
