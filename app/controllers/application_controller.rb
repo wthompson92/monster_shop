fe64b544f72e7050601483d4c8d4ec0051be9c35
username <wnt925@gmail.com>
2019-07-25 10:19:07 -0600

    Add format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
- class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :cart, :current_user, :current_reg_user?, :current_merchant_admin?, :current_admin?, :require_current_user, :deny_admin, :current_merchant_employee?
 
@@ -11,12 +11,12 @@
   def require_current_user
     unless current_user
       render file: "/public/404", status: 404
-  	end
+    end
   end
 
-	def current_merchant_employee?
-		current_user && current_user.user? && current_user.merchant_id
-	end
+  def current_merchant_employee?
+    current_user && current_user.user? && current_user.merchant_id
+  end
 
   def current_merchant_admin?
     current_user && current_user.merchant_admin?
@@ -26,17 +26,17 @@
     current_user && current_user.admin?
   end
 
-	def deny_admin
-		if current_user && current_user.admin?
-			render file: "/public/404", status: 404
-		end
-	end
+  def deny_admin
+    if current_user && current_user.admin?
+      render file: "/public/404", status: 404
+    end
+  end
 
   def cart
     @cart ||= Cart.new(session[:cart])
   end
 
   def pending_order
-        order.pending?
-    end
+    order.pending?
+  end
 end
