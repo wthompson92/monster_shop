a534cf7fa8f5ecaea8bdd1d4f384bd3dd3b2c2a1
username <wnt925@gmail.com>
2019-08-10 14:22:14 -0600

    Add routed

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,32 @@
 Rails.application.routes.draw do
+
   root 'welcome#index'
-  get '/login', to: 'sessions#new', as: 'login'
-  post '/login', to: 'sessions#create', as: 'register'
-  delete '/logout', to: 'sessions#destroy', as: 'logout'
+  get '/login', to: 'sessions#new', as: :login
+  post '/login', to: 'sessions#create', as: :register
+  delete '/logout', to: 'sessions#destroy', as: :logout
 
-  resources :merchants do
-    resources :items, only: [:index, :new, :create, :update]
-  end
+	get '/merchants', to: 'merchants#index', as: :merchants
+	get '/merchants/new', to: 'merchants#new', as: :new_merchant
+	post '/merchants', to: 'merchants#create'
+	get '/merchants/:id', to: 'merchants#show', as: :merchant
+	get '/merchants/:id/edit', to: 'merchants#edit', as: :edit_merchant
+	patch '/merchants/:id', to: 'merchants#update'
+	delete '/merchants/:id', to: 'merchants#destroy'
 
-  resources :items, only: [:index, :show] do
-    resources :reviews, only: [:new, :create]
-  end
+	get '/merchants/:merchant_id/items', to: 'items#index', as: :merchant_items
+	get '/merchants/:id/items/new', to: 'items#new', as: :new_merchant_item
+	post '/merchants/:id/items', to: 'items#create'
+	patch '/merchants/:id/items/:item_id', to: 'items#update', as: :merchant_item
 
-  resources :reviews, only: [:edit, :update, :destroy]
+	get '/items', to: 'items#index', as: :items
+	get '/items/:id', to: 'items#show', as: :item
+
+	get '/items/:item_id/reviews/new', to: 'reviews#new', as: :new_item_review
+	post '/items/:item_id/reviews', to: 'reviews#create', as: :item_reviews
+
+	get '/reviews/:id/edit', to: 'reviews#edit', as: :edit_review
+	patch '/reviews/:id', to: 'reviews#update', as: :review
+	delete '/reviews/:id', to: 'reviews#destroy'
 
   get '/cart', to: 'cart#show', as: 'cart'
 	post '/cart/:item_id', to: 'cart#add_item', as: 'add_to_cart'
@@ -21,9 +35,16 @@ Rails.application.routes.draw do
   delete '/cart/:item_id', to: 'cart#remove_item', as: 'remove_item'
 
   get '/register/new', to: 'users#new', as: :new_user
-  resources :users, only: [:create, :show, :edit, :update] do
-  	resources :orders, only: [:create, :show]
-  end
+
+
+	post '/users', to: 'users#create'
+	get '/users/:id', to: 'users#show', as: :user
+	get '/users/:id/edit', to: 'users#edit', as: :edit_user
+	patch '/users/:id', to: 'users#update'
+
+	post '/users/:user_id/orders', to: 'orders#create', as: :user_orders
+	get '/users/:user_id/orders/:order_id', to: 'orders#show', as: :user_order
+
 
   get '/profile', to: 'users#show', as: :profile
   get '/profile/orders', to: 'orders#index', as: :profile_orders
@@ -38,18 +59,14 @@ Rails.application.routes.draw do
 
   namespace :merchant_admins do
     resources :items, only: [:new, :create, :edit, :update, :destroy]
-		resources :users
-  end
-  namespace :merchant_admins do
-    resources :items, only: [:update, :new, :create]
     resources :orders, only: [:show, :update]
   end
+
   patch '/admin/orders/:id', to: 'admin/orders#update', as: :ship_order
 
   namespace :admin do
-    resources :users
+    resources :users, only: [:index, :show]
   end
-
 
 	get '/merchant/items', to: 'merchant_admins/items#index', as: :only_merchants_items
 
